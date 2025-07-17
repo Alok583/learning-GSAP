00c52b300c30c4f7411ff7bd37ae3949925e9d69
Alok Kumar Rout <alokkurout583@gmail.com>
2025-07-17 15:04:51 +0530

    rotating box

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    rotate:360,
+    delay:1,
+
+})
+gsap.from("#page2 #box",{
+    scale:0,
+    rotate:360,
+    delay:1,
+
+})
+gsap.from("#page3 #box",{
+    scale:0,
+    rotate:360,
+    delay:1,
+
+})
